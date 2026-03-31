b4812cb0cafbea9a6856342df3326e78ee601a8a
Fustigate8933 <ianchang891@gmail.com>
2026-03-30 21:34:37 -0400

    .

--- .config/eww/scripts/change_volume.sh
+++ .config/eww/scripts/change_volume.sh
@@ -5,7 +5,7 @@ volume=$(pamixer --get-volume-human | tr -d '%')
 if [ "$direction" = "down" ]; then
     pactl -- set-sink-volume 0 -5%
 elif [ "$direction" = "up" ]; then
-    if [ "$volume" -lt 146 ]; then
+    if [ "$volume" -lt 96 ]; then
 #        new_volume=$(( $(pactl list sinks | grep 'Volume:' | head -n 1 | sed -e 's/.* \([0-9]\+\)% .*/\1/') + 5 ))
 #        pactl -- set-sink-volume 0 "$(( new_volume > 100 ? 100 : new_volume ))%"
       pactl -- set-sink-volume 0 +5%
